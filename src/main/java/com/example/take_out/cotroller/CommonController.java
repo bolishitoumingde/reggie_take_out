073ef2258da3ac927e9e22358a409e5e07484aa4
bolishitoumingde <li.jian.boli@qq.com>
2022-08-17 13:17:34 +0800

    新增文件上传

--- src/main/java/com/example/take_out/cotroller/CommonController.java
+++ src/main/java/com/example/take_out/cotroller/CommonController.java
@@ -2,10 +2,16 @@ package com.example.take_out.cotroller;
 
 import com.example.take_out.cotroller.utils.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 文件上传下载
@@ -16,9 +22,22 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/common")
 public class CommonController {
 
+    @Value("${upload.path}")
+    private String path;
+
     @PostMapping("/upload")
-    public R<String> upload(MultipartFile file) {
+    public R<String> upload(@RequestPart("file") MultipartFile file) {
         log.info("文件上传");
+        String originalFilename = file.getOriginalFilename();
+        // 获取后缀
+        assert originalFilename != null;
+        String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString() + substring;
+        try {
+            file.transferTo(new File(path + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 }
