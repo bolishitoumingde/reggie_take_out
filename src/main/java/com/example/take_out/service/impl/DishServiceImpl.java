c077d9c5c393601d3d5d76fa00d98c4bc971b311
bolishitoumingde <li.jian.boli@qq.com>
2022-08-17 17:01:26 +0800

    删除文件

--- src/main/java/com/example/take_out/service/impl/DishServiceImpl.java
+++ src/main/java/com/example/take_out/service/impl/DishServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.take_out.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.take_out.cotroller.utils.R;
 import com.example.take_out.entity.Dish;
@@ -8,6 +10,8 @@ import com.example.take_out.entity.DishFlavor;
 import com.example.take_out.service.IDishFlavorService;
 import com.example.take_out.service.IDishService;
 import com.example.take_out.mapper.DishMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,12 +24,23 @@ import java.util.stream.Collectors;
  * @description 针对表【dish(菜品管理)】的数据库操作Service实现
  * @createDate 2022-08-17 10:57:25
  */
+@Slf4j
 @Service
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements IDishService {
 
     @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
     private IDishFlavorService dishFlavorService;
 
+    /**
+     * 添加新菜品
+     *
+     * @param dishDto 菜品信息
+     * @return 成功
+     */
+    // TODO 图片信息为空
     @Override
     @Transactional
     public R<String> addDish(DishDto dishDto) {
@@ -43,6 +58,20 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements ID
         // 保存口味信息
         dishFlavorService.saveBatch(flavors);
         return R.success("添加成功");
+    }
+
+    @Override
+    public R<Page<Dish>> getPage(int currentPage, int pageSize, String name) {
+        LambdaQueryWrapper<Dish> lqw = new LambdaQueryWrapper<>();
+        lqw.like(Strings.isNotEmpty(name), Dish::getName, name);
+        lqw.orderByDesc(Dish::getSort);
+        Page<Dish> page = new Page<>();
+        this.page(page, lqw);
+        List<Dish> records = page.getRecords();
+        for (Dish record : records) {
+            System.out.println(record);
+        }
+        return R.success(page);
     }
 }
 
