73b5f76a4ff19898a1b4f78dddffe5e757378f13
bolishitoumingde <li.jian.boli@qq.com>
2022-08-18 10:02:09 +0800

    新增地址簿部分功能

---
+++ src/main/java/com/example/take_out/cotroller/AddressBookController.java
@@ -0,0 +1,63 @@
+package com.example.take_out.cotroller;
+
+
+import com.example.take_out.cotroller.utils.R;
+import com.example.take_out.entity.AddressBook;
+import com.example.take_out.service.IAddressBookService;
+import com.example.take_out.utils.BaseContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/addressBook")
+public class AddressBookController {
+
+    @Autowired
+    private IAddressBookService addressBookService;
+
+    /**
+     * 新增地址
+     *
+     * @param addressBook 地址类
+     * @return 成功
+     */
+    @PostMapping
+    public R<String> addAddress(@RequestBody AddressBook addressBook) {
+        Long id = BaseContext.getId();
+        addressBook.setUserId(id);
+        addressBookService.save(addressBook);
+        return R.success("添加成功");
+    }
+
+
+    /**
+     * 修改默认地址
+     *
+     * @param addressBook 地址类
+     * @return 成功
+     */
+    @PutMapping("/default")
+    public R<String> setDefault(@RequestBody AddressBook addressBook) {
+        return addressBookService.setDefault(addressBook);
+    }
+
+    /**
+     * 根据id查询地址
+     *
+     * @param id id
+     * @return 地址信息
+     */
+    @GetMapping("/{id}")
+    public R<Object> getById(@PathVariable Long id) {
+        AddressBook addressBook = addressBookService.getById(id);
+        if (addressBook != null) {
+            return R.success(addressBook);
+        }
+        return R.error("未找到该信息");
+    }
+
+
+
+}
--- src/main/java/com/example/take_out/service/impl/AddressBookServiceImpl.java
+++ src/main/java/com/example/take_out/service/impl/AddressBookServiceImpl.java
@@ -1,20 +1,36 @@
 package com.example.take_out.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.take_out.cotroller.utils.R;
 import com.example.take_out.entity.AddressBook;
-import com.example.take_out.service.AddressBookService;
+import com.example.take_out.service.IAddressBookService;
 import com.example.take_out.mapper.AddressBookMapper;
+import com.example.take_out.utils.BaseContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
-* @author Administrator
-* @description 针对表【address_book(地址管理)】的数据库操作Service实现
-* @createDate 2022-08-18 09:39:16
-*/
+ * @author Administrator
+ * @description 针对表【address_book(地址管理)】的数据库操作Service实现
+ * @createDate 2022-08-18 09:39:16
+ */
 @Service
 public class AddressBookServiceImpl extends ServiceImpl<AddressBookMapper, AddressBook>
-    implements AddressBookService{
+        implements IAddressBookService {
 
+    @Override
+    @Transactional
+    public R<String> setDefault(AddressBook addressBook) {
+        LambdaUpdateWrapper<AddressBook> lqw = new LambdaUpdateWrapper<>();
+        lqw.eq(AddressBook::getUserId, BaseContext.getId());
+        lqw.set(AddressBook::getIsDefault, 0);
+        this.update(lqw);
+
+        addressBook.setIsDefault(1);
+        this.updateById(addressBook);
+        return R.success("修改成功");
+    }
 }
 
 
